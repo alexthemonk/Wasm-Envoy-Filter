1e606a9a99a744ed56a6a265377ad357220b3b63
Alex Liu <alliu@riotgames.com>
2022-02-22 10:42:44 -0800

    update wasm filter

--- test-wasm-plugin/main.go
+++ test-wasm-plugin/main.go
@@ -8,32 +8,41 @@ import (
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 )
 
-const tickMilliseconds uint32 = 1000
+const (
+	tickMilliseconds uint32 = 1000
+)
+
+type (
+	vmContext struct {
+		// Embed the default VM context here,
+		// so that we don't need to reimplement all the methods.
+		types.DefaultVMContext
+	}
+
+	pluginContext struct {
+		// Embed the default plugin context here,
+		// so that we don't need to reimplement all the methods.
+		types.DefaultPluginContext
+	}
+
+	httpContext struct {
+		// Embed the default plugin context here,
+		// so that we don't need to reimplement all the methods.
+		types.DefaultHttpContext
+	}
+)
 
 func main() {
 	proxywasm.SetVMContext(&vmContext{})
 }
 
-type vmContext struct {
-	// Embed the default VM context here,
-	// so that we don't need to reimplement all the methods.
-	types.DefaultVMContext
-}
-
 // Override types.DefaultVMContext.
 func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
-	return &helloWorld{}
-}
-
-type helloWorld struct {
-	// Embed the default plugin context here,
-	// so that we don't need to reimplement all the methods.
-	types.DefaultPluginContext
-	contextID uint32
+	return &pluginContext{}
 }
 
 // Override types.DefaultPluginContext.
-func (ctx *helloWorld) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
+func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
 	rand.Seed(time.Now().UnixNano())
 
 	proxywasm.LogInfo("OnPluginStart from Go!")
@@ -45,11 +54,18 @@ func (ctx *helloWorld) OnPluginStart(pluginConfigurationSize int) types.OnPlugin
 }
 
 // Override types.DefaultPluginContext.
-func (ctx *helloWorld) OnTick() {
+func (ctx *pluginContext) OnTick() {
 	t := time.Now().UnixNano()
 	proxywasm.LogInfof("It's %d: random value: %d", t, rand.Uint64())
 	proxywasm.LogInfof("OnTick called")
 }
 
 // Override types.DefaultPluginContext.
-func (*helloWorld) NewHttpContext(uint32) types.HttpContext { return &types.DefaultHttpContext{} }
+func (*pluginContext) NewHttpContext(uint32) types.HttpContext {
+	return &httpContext{}
+}
+
+func (ctx *httpContext) OnHttpRequestHeaders(int, bool) types.Action {
+	proxywasm.LogInfof("OnHttpRequestHeaders with ctx %v", ctx)
+	return types.ActionContinue
+}
