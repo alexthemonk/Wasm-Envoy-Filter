a4dea1adb280dd8ebfc79553ee6371caf63a3f5b
Alex Liu <alliu@riotgames.com>
2022-02-22 10:45:36 -0800

    update wasm filter

--- test-wasm-plugin/main.go
+++ test-wasm-plugin/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 )
@@ -39,25 +36,6 @@ func main() {
 // Override types.DefaultVMContext.
 func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
 	return &pluginContext{}
-}
-
-// Override types.DefaultPluginContext.
-func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
-	rand.Seed(time.Now().UnixNano())
-
-	proxywasm.LogInfo("OnPluginStart from Go!")
-	if err := proxywasm.SetTickPeriodMilliSeconds(tickMilliseconds); err != nil {
-		proxywasm.LogCriticalf("failed to set tick period: %v", err)
-	}
-
-	return types.OnPluginStartStatusOK
-}
-
-// Override types.DefaultPluginContext.
-func (ctx *pluginContext) OnTick() {
-	t := time.Now().UnixNano()
-	proxywasm.LogInfof("It's %d: random value: %d", t, rand.Uint64())
-	proxywasm.LogInfof("OnTick called")
 }
 
 // Override types.DefaultPluginContext.
